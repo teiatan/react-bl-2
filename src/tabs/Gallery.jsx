f0de46d50b48c6a49b3cb1b932f782c773e17cf0
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-03-26 16:00:07 +0300

    finish

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -1,7 +1,8 @@
 import { Component } from 'react';
 
 import * as ImageService from 'service/image-service';
-import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
+import { Button, SearchForm, Grid, GridItem, Text, CardItem, Loader } from 'components';
+
 
 export class Gallery extends Component {
 
@@ -10,21 +11,42 @@ export class Gallery extends Component {
     page: 1,
     photos: [],
     showBtn: false,
+    error: null,
+    isEmpty: false,
+    isLoading: false
   }
   componentDidUpdate(prevProps, prevState) {
+
     if (this.state.search !== prevState.search || this.state.page !== prevState.page) {
+      this.setState({ isLoading: true })
       ImageService.getImages(this.state.search, this.state.page).then(data => {
+        if (data.photos.length === 0) {
+          this.setState({ isEmpty: true })
+        }
         console.log(data);
         this.setState((prevState) => ({
           photos: [...prevState.photos, ...data.photos],
           showBtn: this.state.page < Math.ceil(data.total_results / 15)
         }))
-      })
+
+
+      }).catch(err => (
+        this.setState({ error: err })
+      ))
+        .finally(
+          this.setState({ isLoading: false })
+        )
     }
   }
 
   handleFormSubmit = (data) => {
-    this.setState({ search: data, photos: [], page: 1 });
+    this.setState({
+      search: data,
+      photos: [],
+      page: 1,
+      error: null,
+      isEmpty: false
+    });
   };
   handleLoadMore = () => {
     this.setState(prev => ({
@@ -32,10 +54,11 @@ export class Gallery extends Component {
     }))
   }
   render() {
-    const { photos, showBtn } = this.state;
+    const { photos, showBtn, isEmpty, error, isLoading } = this.state;
     return (
       <>
-        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        {isEmpty && <Text textAlign="center">Sorry. There are no images ... 😭</Text>}
+        {error && <Text textAlign="center">Sorry. There is an Error ... 😭</Text>}
         <SearchForm onSubmit={this.handleFormSubmit} />
         <Grid>
           {photos.map(photo => {
@@ -47,7 +70,9 @@ export class Gallery extends Component {
           })}
           {showBtn && <Button onClick={this.handleLoadMore}>Load more</Button>}
 
+
         </Grid>
+        {isLoading && <Loader />}
       </>
     );
   }
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -3,7 +3,19 @@ import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
 export class Todos extends Component {
+  state = {
+    items: [],
+  }
+
+  onSubmit = (text) => {
+    console.log(text);
+    const todo = { text, id: nanoid(4) }
+    console.log(todo);
+    this.setState(prev => ({ items: [todo, ...prev.items] }))
+  }
   render() {
-    return <Text>Todos</Text>;
+    return (
+      <SearchForm onSubmit={this.onSubmit} />
+    )
   }
 }
