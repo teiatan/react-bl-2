8c3a26613ba287c82f4a24fd70587efdc484168f
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-26 16:18:38 +0300

    to do delete logic

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -2,6 +2,14 @@ import { Text } from 'components';
 import { TodoWrapper, DeleteButton, EditButton } from './Todo.styled';
 import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
 
-export const Todo = () => {
-  return <h2>Todo</h2>;
+export const Todo = ({id, text, counter, onDelete}) => {
+  return <TodoWrapper>
+      <Text textAlign="center" marginBottom="20px">
+        TODO #{counter}
+      </Text>
+      <Text>{text}</Text>
+      <DeleteButton onClick={()=>onDelete(id)} type="button">
+        <RiDeleteBinLine size={24} />
+      </DeleteButton>
+  </TodoWrapper>;
 };
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -7,15 +7,47 @@ export class Todos extends Component {
     items: [],
   }
 
+  componentDidMount() {
+    const localItems = JSON.parse(localStorage.getItem('items'));
+    if(localItems) {
+      this.setState({items:localItems})
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(this.state.items.length !== prevState.items) {
+      localStorage.setItem('items', JSON.stringify(this.state.items))
+    }
+  }
+
   onSubmit = (text) => {
     console.log(text);
     const todo = { text, id: nanoid(4) }
     console.log(todo);
     this.setState(prev => ({ items: [todo, ...prev.items] }))
   }
+
+  onDelete = (id) => {
+    this.setState(prev => ({
+      items: prev.items.filter(item => item.id !== id)
+    }))
+  }
+
   render() {
     return (
+      <>
       <SearchForm onSubmit={this.onSubmit} />
+      <Grid>
+          {this.state.items.map((item, index) => {
+            return <GridItem key={item.id}>
+                <Todo id={item.id} text={item.text} counter={index+1} onDelete={this.onDelete}/> 
+            </GridItem>
+          })}
+
+
+        </Grid>
+      </>  
     )
+    
   }
 }
