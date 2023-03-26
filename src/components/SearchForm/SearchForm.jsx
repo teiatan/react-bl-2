990b2676ea8798969897957e9d2dd9a15f3c325a
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-26 14:50:30 +0300

    add search form

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,32 @@ import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
+  state ={
+    value: "",
+  }
+
+  handleInput = e => {
+    this.setState({value:e.target.value})
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.value);
+  }
+
   render() {
-    return <h2>SearchForm</h2>;
+    return <SearchFormStyled onSubmit={this.handleSubmit}>
+    <FormBtn type="submit">
+      <FiSearch size="16px" />
+    </FormBtn>
+    <InputSearch
+    onChange={this.handleInput}
+      placeholder="What do you want to write?"
+      name="search"
+      required
+      autoFocus
+      value={this.state.value}
+    />
+  </SearchFormStyled>
   }
 }
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -4,10 +4,22 @@ import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
+
+  state = {
+    search: "",
+    page: 1,
+    
+  }
+
+  handleFormSubmit = (data) => {
+    this.setState({search: data});
+  };
+
   render() {
     return (
       <>
         <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        <SearchForm onSubmit={this.handleFormSubmit}/>
       </>
     );
   }
