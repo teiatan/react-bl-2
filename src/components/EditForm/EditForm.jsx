f6615ae419fa4ead31cc4f0af7f6542dbb17e493
DenMerch <rosignolli@gmail.com>
2023-03-26 16:44:33 +0300

    den

--- src/components/EditForm/EditForm.jsx
+++ src/components/EditForm/EditForm.jsx
@@ -4,6 +4,23 @@ import { MdOutlineCancel } from 'react-icons/md';
 import { SearchFormStyled, FormBtn, InputSearch } from 'components';
 import { BtnEdit } from './EditForm.styled';
 
-export const EditForm = () => {
-  return <h2>Edit Form</h2>;
+export const EditForm = ({ currentTodo, onUpdate, onChange, onCancel }) => {
+  return (<SearchFormStyled onSubmit={onUpdate}>
+    <BtnEdit type="button" onClick={onCancel}>
+      <MdOutlineCancel size="16px" color="red" />
+    </BtnEdit>
+
+    <FormBtn type="submit">
+      <RiSaveLine size="16px" color="green" />
+    </FormBtn>
+
+    <InputSearch
+      placeholder="EDIT TODO"
+      name="edit"
+      required
+      defaultValue={currentTodo.text}
+      autoFocus
+      onChange={onChange}
+    />
+  </SearchFormStyled>)
 };
